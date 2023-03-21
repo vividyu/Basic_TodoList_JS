af8ff41ad6d1ee5877695a8515216834810276d4
Samuel <sixgod666@gmail.com>
2023-03-21 11:12:16 -0500

    rename

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function get() {
+    // const axios = require('axios/dist/browser/axios.cjs'); // browser
+    // let api_key = "2f39ac8abf607fbbc583ce393c0f56f3";
+    // let res = axios(`https://api.themoviedb.org/3/movie/550?api_key=${api_key}`);
+    alert("hi");
+}
+
+get();
