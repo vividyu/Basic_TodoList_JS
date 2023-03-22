016a85cae57c7a2d91967a56da4fb1165e13bca5
Guanyu Hou <10951700+vividyu@users.noreply.github.com>
2023-03-21 23:54:50 -0500

    cp2

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const api_key = "2f39ac8abf607fbbc583ce393c0f56f3";
 const BASE_URL = `https://api.themoviedb.org/3/trending/movie/week?`;
 const CONFIG_URL = `https://api.themoviedb.org/3/configuration?api_key=${api_key}`;
 
-const getImgUrlPrefix = async () => {
+const getImgUrlPrefix = async (imgsize) => {
   try {
     const config = await axios.get(CONFIG_URL);
     const cfgdata = config.data.images;
-    const imgUrlPrefix = cfgdata.base_url + cfgdata.poster_sizes[4];
+    const imgUrlPrefix = cfgdata.base_url + cfgdata.poster_sizes[imgsize];
 
     return imgUrlPrefix;
 
@@ -24,10 +24,12 @@ const getMovieInfo = async (page) => {
     const imageContainer = document.querySelector('.image-container');
     imageContainer.innerHTML = '';
 
-    const imgUrlPrefix = await getImgUrlPrefix();
+    const imgsize = 4;//w500
+    const imgUrlPrefix = await getImgUrlPrefix(imgsize);
 
     const listUrl = `${BASE_URL}page=${page}&api_key=${api_key}`;
     const response = await axios.get(listUrl);
+    console.log(response);
 
     const res = response.data.results;
     const total_pages = response.data.total_pages;
@@ -35,10 +37,31 @@ const getMovieInfo = async (page) => {
 
     res.forEach(detail => {
       const imgUrl = detail.poster_path;
+      const movieid = detail.id;
+      const movietitle = detail.title;
+      const movie_release_date = detail.release_date;
+
+      const item = document.createElement('div');
       const image = document.createElement('img');
+      const title = document.createElement('p');
+      const release_date = document.createElement('p');
+
+      item.className = `movie-item`;
+
       image.src = imgUrlPrefix + imgUrl;
       image.classList.add('image');
-      imageContainer.appendChild(image);
+      image.className = `img${movieid}`;
+
+      title.textContent = movietitle;
+      title.className = `movie-title`;
+
+      release_date.textContent = movie_release_date;
+      release_date.className = `release-date`;
+
+      imageContainer.appendChild(item);
+      item.appendChild(image);
+      item.appendChild(title);
+      item.appendChild(release_date);
     })
 
     return { total_pages, total_results };
@@ -85,30 +108,32 @@ const pagination = async () => {
   });
 }
 
-const tab = document.addEventListener('DOMContentLoaded', function () {
-  const tabLinks = document.querySelectorAll('.tab-link');
-  const tabContents = document.querySelectorAll('.tab-content');
+function loadTabs() {
+  const tabctrl = document.addEventListener('DOMContentLoaded', function () {
+    const tabLinks = document.querySelectorAll('.tab-link');
+    const tabContents = document.querySelectorAll('.tab-content');
 
-  tabLinks.forEach(link => {
-    link.addEventListener('click', function (event) {
-      event.preventDefault();
+    tabLinks.forEach(link => {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
 
-      tabLinks.forEach(link => link.classList.remove('active'));
-      event.target.classList.add('active');
+        tabLinks.forEach(link => link.classList.remove('active'));
+        event.target.classList.add('active');
 
-      const tabId = event.target.getAttribute('data-tab');
-      tabContents.forEach(content => {
-        content.style.display = content.id === `tab-${tabId}` ? 'block' : 'none';
+        const tabId = event.target.getAttribute('data-tab');
+        tabContents.forEach(content => {
+          content.style.display = content.id === `tab-${tabId}` ? 'block' : 'none';
+        });
+
       });
-
     });
+    // Activate the first tab by default
+    tabLinks[0].click();
   });
-
-  // Activate the first tab by default
-  tabLinks[0].click();
-});
+}
 
 const main = async () => {
+  loadTabs();
   pagination();
 }
 main();
