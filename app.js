45b88bdced8fcbaef8462dda2388e79227138910
Samuel <sixgod666@gmail.com>
2023-03-22 11:47:58 -0500

    add loading gif

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const getImgUrlPrefix = async (poster_sz = 4, bg_sz = 2) => {
     const config = await axios.get(CONFIG_URL);
     const cfgdata = config.data.images;
     console.log(cfgdata);//test log
-    
+
     const posterUrlPrefix = cfgdata.secure_base_url + cfgdata.poster_sizes[poster_sz];
     const bgUrlPrefix = cfgdata.secure_base_url + cfgdata.backdrop_sizes[bg_sz];
 
@@ -22,10 +22,54 @@ const getImgUrlPrefix = async (poster_sz = 4, bg_sz = 2) => {
   }
 };
 
-const getMovieInfo = async (page) => {
+const generatePosterFrame = async (page = 1) => {
   try {
     const imageContainer = document.querySelector('.image-container');
     imageContainer.innerHTML = '';
+
+    const listUrl = `${BASE_URL}page=${page}&api_key=${api_key}`;
+    const response = await axios.get(listUrl);
+    console.log(response);//test log
+
+    const res = response.data.results;
+    const total_pages = response.data.total_pages;
+    const total_results = response.data.total_results;
+
+    res.forEach(detail => {
+      const item = document.createElement('div');
+      const image = document.createElement('img');
+      const title = document.createElement('p');
+      const release_date = document.createElement('p');
+
+      item.className = `movie-item`;
+
+      image.src = 'https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif';
+      image.classList.add('image');
+      image.className = `img_loading`;
+
+      title.textContent = '';
+      title.className = `movie-title`;
+
+      release_date.textContent = '';
+      release_date.className = `release-date`;
+
+      imageContainer.appendChild(item);
+      item.appendChild(image);
+      item.appendChild(title);
+      item.appendChild(release_date);
+    })
+
+    return { total_pages, total_results };
+
+  } catch (errors) {
+    console.error(errors);
+  }
+};
+
+const updateMovieInfo = async (page = 1) => {
+  try {
+    const imageContainer = document.querySelector('.image-container');
+    //imageContainer.innerHTML = '';
 
     const poster_sz = 4;//w500
     const { posterUrlPrefix } = await getImgUrlPrefix(poster_sz);
@@ -38,37 +82,24 @@ const getMovieInfo = async (page) => {
     const total_pages = response.data.total_pages;
     const total_results = response.data.total_results;
 
-    res.forEach(detail => {
-      const imgUrl = detail.poster_path;
-      const movieid = detail.id;
-      const movietitle = detail.title;
-      const movie_release_date = detail.release_date;
+    const len = res.length;
+    for (let i = 0; i < len; i++) {
+      const imgUrl = res[i].poster_path;
+      const movieid = res[i].id;
+      const movietitle = res[i].title;
+      const movie_release_date = res[i].release_date;
+      const movieItemAll = document.querySelectorAll('.movie-item');
+      const poster = movieItemAll[i].querySelector('img');
+      const title = movieItemAll[i].querySelector('.movie-title');
+      const release_date = movieItemAll[i].querySelector('.release-date');
 
-      const item = document.createElement('div');
-      const image = document.createElement('img');
-      const title = document.createElement('p');
-      const release_date = document.createElement('p');
-
-      item.className = `movie-item`;
-
-      image.src = posterUrlPrefix + imgUrl;
-      image.classList.add('image');
-      image.className = `img${movieid}`;
+      poster.src = posterUrlPrefix + imgUrl;
+      poster.className = `img${movieid}`;
 
       title.textContent = movietitle;
-      title.className = `movie-title`;
-
       release_date.textContent = movie_release_date;
-      release_date.className = `release-date`;
-
-      imageContainer.appendChild(item);
-      item.appendChild(image);
-      item.appendChild(title);
-      item.appendChild(release_date);
-    })
-
+    }
     return { total_pages, total_results };
-
   } catch (errors) {
     console.error(errors);
   }
@@ -86,7 +117,7 @@ const pagination = async () => {
   //get page 1 by default
   pageNum.textContent = page;
 
-  const { total_pages, total_results } = await getMovieInfo(page);
+  const { total_pages, total_results } = await updateMovieInfo(page);
   TotalPages.textContent = total_pages;
   TotalRes.textContent = total_results;
 
@@ -98,7 +129,7 @@ const pagination = async () => {
     pageNum.textContent = page;
     prevButton.disabled = page <= 1;
     nextButton.disabled = page >= 1000;
-    await getMovieInfo(page);
+    await updateMovieInfo(page);
   });
 
   // Add event listener to the next button
@@ -107,7 +138,7 @@ const pagination = async () => {
     pageNum.textContent = page;
     prevButton.disabled = page <= 1;
     nextButton.disabled = page >= total_pages;
-    await getMovieInfo(page);
+    await updateMovieInfo(page);
   });
 }
 
@@ -135,9 +166,23 @@ function loadTabs() {
   });
 }
 
+const listenPoster = async () => {
+  const imageContainer = document.querySelector('.image-container');
+  const posterAll = imageContainer.querySelectorAll('img');
+
+  posterAll.forEach(poster => {
+    poster.addEventListener('click', () => {
+      console.log(poster.className);
+    });
+  });
+}
+
 const main = async () => {
   loadTabs();
-  pagination();
+  await generatePosterFrame();
+  await updateMovieInfo();
+  await pagination();
+  await listenPoster();
 }
 main();
 
